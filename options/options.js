83dbfade9f37a167d9b352821d78f7786d53b028
Crowdbotics <engineering@crowdbotics.com>
2023-07-06 23:20:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-tree-dev-98407",
+  url: "https://withered-tree-dev-98407.botics.co",
+  api: "https://withered-tree-dev-98407.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
